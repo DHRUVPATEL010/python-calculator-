cc13afbef376a9570a4fa0e134db14ee229b5bdd
dhruv patel <in.dhruvpatel@gmail.com>
2023-04-09 18:30:27 +0530

    simple calculator

---
+++ simple cal/main.py
@@ -0,0 +1,29 @@
+from newcalci import calci
+
+
+        
+
+
+
+
+
+def choice():
+  e=input("enter c for continue or enter e for exit : ")
+  while e != "c" and e != "e":
+    print("please enter a valide option")
+    e=input("enter c for continue or enter e for exit : ")
+  return e
+
+while choice() == "c":
+ calci()
+ 
+
+
+ 
+     
+     
+     
+
+
+
+
