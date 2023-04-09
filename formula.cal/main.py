5f60b01f53a5b024da57133061d1d5767254c8a6
dhruv patel <in.dhruvpatel@gmail.com>
2023-04-09 18:50:59 +0530

    code formmeted

--- formula.cal/main.py
+++ formula.cal/main.py
@@ -1,29 +1,13 @@
 from newcalci import calci
 
 
-        
-
-
-
-
-
 def choice():
-  e=input("enter c for continue or enter e for exit : ")
-  while e != "c" and e != "e":
-    print("please enter a valide option")
-    e=input("enter c for continue or enter e for exit : ")
-  return e
+    e = input("enter c for continue or enter e for exit : ")
+    while e != "c" and e != "e":
+        print("please enter a valide option")
+        e = input("enter c for continue or enter e for exit : ")
+    return e
+
 
 while choice() == "c":
- calci()
- 
-
-
- 
-     
-     
-     
-
-
-
-
+    calci()
