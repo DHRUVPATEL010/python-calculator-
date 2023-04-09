75fd2732420b6edc9337666816e6afcb0b53d40b
dhruv patel <in.dhruvpatel@gmail.com>
2023-04-09 18:37:16 +0530

    initial formula calculate

---
+++ formula.cal/newcalci.py
@@ -0,0 +1,28 @@
+operations = {
+    "+": lambda x, y: x + y,
+     "-": lambda x, y: x - y,
+     "*": lambda x, y: x * y,
+     "/": lambda x, y: x / y,
+     "//":lambda x, y: x//y,
+     "**":lambda x, y: x ** y,
+     "%":lambda x,y: x%y
+}
+
+def calci():
+
+    a =int(input("Enter first number : "))
+
+    b =int(input("Enter second second : "))
+
+    print("enter what do you want to performe out of this")
+
+    operation = input(list(operations.keys()))
+
+    function = operations.get(operation)
+    
+    if(function):
+        print("Result: ", function(a,b))
+    else:
+        print("enter valid input")
+
+calci()
